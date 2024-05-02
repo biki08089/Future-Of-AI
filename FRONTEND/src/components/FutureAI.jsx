1b4fc1ba8491db26ab27d815bcaa427af01e726f
biki08089 <bikashpradhan99048@gmail.com>
2024-05-02 10:41:16 +0530

    Updated

--- FRONTEND/src/components/FutureAI.jsx
+++ FRONTEND/src/components/FutureAI.jsx
@@ -5,7 +5,7 @@ const FutureAI = () => {
     <div>
     <div data-aos="zoom-in" className='sm:w-[30rem] sm:mx-auto filter h-[3rem] bg-cust-black rounded-3xl mb-2 mx-2 flex justify-center items-center'>
          <div className='h-[2.7rem] w-[99%] bg-black rounded-3xl flex justify-center items-center'>
-            <h1 className='text-cust-white font-semibold'>Future AI</h1>
+            <h1  className='animate-pulseeb text-cust-white font-semibold'>Explore More From Creators</h1>
          </div>
     </div>
 </div>
--- FRONTEND/tailwind.config.js
+++ FRONTEND/tailwind.config.js
@@ -1,35 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    colors:{
-      'cust-white':"white",
-      "dark-green":"#17cad0",
-      "light-sky":"#66ccd0",
-      "dark-sky":"#2196f3",
-      "backgroundColor":"red",
-      "cust-gray":"gray",
-      "cust-black":"#191717",
-      "black":"black",
-      "cust-bg":"#EEF5FF",
-      "cust-lite-black":"#293333",
-      "cust-semiblack":"#3d3a3a",
-      "cust-lightgray":"#ede6e6",
-      "cust-gray2nd":"#DDDDDD",
-      "cust-lite-blue":"#075ff7",
-      "cust-EEEDEB":"#EEEDEB"
-
+    colors: {
+      "cust-white": "white",
+      "dark-green": "#17cad0",
+      "light-sky": "#66ccd0",
+      "dark-sky": "#2196f3",
+      backgroundColor: "red",
+      "cust-gray": "gray",
+      "cust-black": "#191717",
+      black: "black",
+      "cust-bg": "#EEF5FF",
+      "cust-lite-black": "#293333",
+      "cust-semiblack": "#3d3a3a",
+      "cust-lightgray": "#ede6e6",
+      "cust-gray2nd": "#DDDDDD",
+      "cust-lite-blue": "#075ff7",
+      "cust-EEEDEB": "#EEEDEB",
     },
     boxShadow: {
-      '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.2)',   
-     },
-     animation:{
-      "bounceb":"bounce 2s infinite"
-     }
+      "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.2)",
+    },
+    animation: {
+      bounceb: "bounce 2s infinite",
+      pulseeb: "pulse 3s infinite    ",
+    },
   },
   plugins: [],
-}
+};
